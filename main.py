3e7df30f9e9bac303bda1da896e88a2dc7faf7be
cybenko.sergey <cybenko.sergey@gmail.com>
2022-06-04 12:44:34 +0300

    Add Camera movement on field

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 
 WIDTH = 960
-HEIGHT = 1080
+HEIGHT = 1000
 FPS = 60
 BG = (26, 35, 126)
 DARK = (0, 0, 81)
@@ -14,19 +14,26 @@ RED = (244, 67, 54)
 RED_200 = (239, 154, 154)
 GREEN = (0, 255, 0)
 BLUE = (33, 150, 243)
+
+MOVE_AREA = 0.02
+SPEED = 50
+move_field = set()
+
 choose_pos = (WIDTH/2, HEIGHT/2)
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (WIDTH,0)
-
 
 class Player(pygame.sprite.Sprite):
     radius = 100
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
+        size = 30
+        rad = size/2
         self.pos = pos
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(BLUE)
+        self.image = pygame.Surface((size, size), pygame.SRCALPHA)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
+        pygame.draw.circle(self.image, BLUE, (rad, rad), rad)
+        
     
     def update(self):
         self.rect.x += 5
@@ -38,14 +45,30 @@ class Player(pygame.sprite.Sprite):
 
 class Field(pygame.sprite.Sprite):
     def __init__(self):
-        self.dimens = (500, 500)
-        self.pos = (round(WIDTH/2), round(HEIGHT/2))
+        self.dimens = (4000, 4000)
+        self.pos = [0, 0]
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface(self.dimens)
         self.image.fill(DARK)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
-
+        
+    def move_left(self):
+        self.pos[0] = min(self.dimens[0]/2, self.pos[0] + SPEED)
+        self.rect.center = self.pos
+        
+    def move_right(self):
+        self.pos[0] = max(WIDTH-self.dimens[0]/2, self.pos[0] - SPEED)
+        self.rect.center = self.pos
+        
+    def move_top(self):
+        self.pos[1] = min(self.dimens[1]/2, self.pos[1] + SPEED) 
+        self.rect.center = self.pos
+        
+    def move_bot(self):
+        self.pos[1] = max(HEIGHT-self.dimens[1]/2, self.pos[1] - SPEED)
+        self.rect.center = self.pos
+        
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -54,8 +77,10 @@ clock = pygame.time.Clock()
 players_sprites = pygame.sprite.Group()
 interface_sprites = pygame.sprite.Group()
 field = Field()
+
+
 interface_sprites.add(field)
-for i in range(4):
+for i in range(200):
     players_sprites.add(Player((
         random.randint(0, field.dimens[0]), 
         random.randint(0, field.dimens[1])
@@ -78,6 +103,7 @@ def is_mouse_intersect_sprites(sprites):
     return False
 
 
+
 running = True
 while running:
     screen.fill(BG)
@@ -90,6 +116,21 @@ while running:
             
         if event.type == pygame.MOUSEMOTION:
             choose_pos = event.pos
+            
+            move_field = set()
+            if WIDTH*MOVE_AREA > event.pos[0]:
+                move_field.add(field.move_left)
+            if (WIDTH - WIDTH*MOVE_AREA) < event.pos[0]:
+                move_field.add(field.move_right)
+            if HEIGHT*MOVE_AREA > event.pos[1]:
+                move_field.add(field.move_top)
+            if (HEIGHT - HEIGHT*MOVE_AREA) < event.pos[1]:
+                move_field.add(field.move_bot)
+    
+    for move in move_field:
+        move()
+            
+            
             
     draw_sprites_areas(players_sprites)
     if is_mouse_intersect_sprites(players_sprites):
