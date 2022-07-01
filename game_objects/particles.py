7df9daac9cf918abe271992fef502d07a5a158ea
cybenko.sergey <cybenko.sergey@gmail.com>
2022-07-01 08:21:46 +0300

    Fix Collision

--- game_objects/particles.py
+++ game_objects/particles.py
@@ -9,8 +9,8 @@ from core import helpers as ch
 
 class LightBeem(Particle):
     light_density = 2
-    damage = 50
-    distance = light_density * damage * 10
+    damage = 10
+    distance = light_density * damage * 10 * 5
     fire = False
 
     def __init__(self, pos, angle):
@@ -44,17 +44,32 @@ class LightBeem(Particle):
                 if collision is None:
                     collision = col
                     collided_spr = spr
-                elif (math.hypot(*collision) > math.hypot(*col)):
+                elif (math.hypot(
+                        (self.distance//2)-collision[0], (self.distance//2)-collision[1]) >
+                        math.hypot((self.distance//2)-col[0], (self.distance//2)-col[1])):
                     collision = col
                     collided_spr = spr
 
         if collision:
+            start_pos = list(self.rect.topleft)
+            rect_start = [0, 0]
+            rect_end = list(collision)
+            if collision[0] < self.distance//2:
+                start_pos[0] += collision[0]
+                rect_start[0] += collision[0]
+                rect_end[0] = self.distance//2
+            if collision[1] < self.distance//2:
+                start_pos[1] += collision[1]
+                rect_start[1] += collision[1]
+                rect_end[1] = self.distance//2
+
             collided_spr.receive_damage(
                 state, round(state.seconds * self.damage))
             state.screen.blit(
                 self.image, state.bg.bg_pos_to_abs(
-                    *self.rect.topleft))
-
+                    *start_pos),
+                (*rect_start, *rect_end)
+            )
         else:
             state.screen.blit(
                 self.image, state.bg.bg_pos_to_abs(
